afce587ac57b224052eaa345c745df693669b0c9
Jandrade2k <dev.jandrade@gmail.com>
2020-02-09 03:35:55 -0300

    backend estruturado - beijo

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,17 @@
 const express = require('express')
+const multer = require('multer')
+const multerConfig = require('./config/multer')
+
 
 const routes = express.Router();
+
+const boxController = require('./controllers/boxController')
+const fileController = require('./controllers/fileController')
+
+routes.post('/boxes', boxController.store);
+routes.post('/boxes/:id/files', multer(multerConfig).single('file'), fileController.store);
+
+routes.get('/boxes/:id', boxController.show)
 
 routes.get('/teste', (req, res) => {
     return res.send('Teste')
